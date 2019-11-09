69c245fa8c1219089c2bb960fc4f9ec9f89af5cd
Radcom <49617699+radcomsms@users.noreply.github.com>
2019-11-09 13:14:47 +0330

    Add files via upload

---
+++ jobsCount.php
@@ -0,0 +1,30 @@
+<?php
+
+
+
+		$url = "https://ippanel.com/services.jspd";
+		
+		$trn_ls = array('');
+		$param = array
+					(
+						'uname'=>'',
+						'pass'=>'',
+						'trendid'=>json_encode($trn_ls),
+						'op'=>'jobscount'
+					);
+					
+		$handler = curl_init($url);             
+		curl_setopt($handler, CURLOPT_CUSTOMREQUEST, "POST");
+		curl_setopt($handler, CURLOPT_POSTFIELDS, $param);                       
+		curl_setopt($handler, CURLOPT_RETURNTRANSFER, true);
+		$response2 = curl_exec($handler);
+		
+		$response2 = json_decode($response2);
+		$res_code = $response2[0];
+		$res_data = $response2[1];
+		
+		
+		echo $res_data;
+
+
+?>
